36a221f8a990cd784964661dc91ab5133947ab6f
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-01-27 14:40:27 -0800

    Update ais.py

--- ais.py
+++ ais.py
@@ -1,59 +1,59 @@
 import json
 import pickle
-import numpy
+import numpy as np
 
 data = json.load(open("idiom.json", "r"))
-idioms = {}
-for d in data:
-  d['first'] = d['pinyin'].split()[0]
-  d['last'] = d['pinyin'].split()[-1]
-  idioms[d['word']] = d
+N = len(data)
+words = [d['word'] for d in data]
+pinyins = [d['pinyin'] for d in data]
+firsts = [p.split()[0] for p in pinyins]
+lasts = [p.split()[-1] for p in pinyins]
 
-pinyins = []
-for i in idioms.values():
-  pinyins.append(i['first'])
-  pinyins.append(i['last'])
-
-pinyins = set(pinyins)
-
+pinyins = set(firsts + lasts)
 index = {}
 for pinyin in pinyins:
   index[pinyin] = []
-  for i in idioms.values():
-    if i['first'] == pinyin:
+  for i in range(N):
+    if firsts[i] == pinyin:
       index[pinyin].append(i)
 
-def find(idex, key):
-  results = index[key]
-  for r in results:
-    print(r['word'])
-
-### initialize
-for d in data:
-  d['winrate'] = 0.5
-  d['visits'] = 0
-
-def choose(key, excludes):
+def choose(key, player, excludes):
   candidates = index[key]
   canditates = set(candidates) - set(excludes)
   if candidates == []:
-    return 0
-  winrates = np.array([c['winrate'] for c in candidates])
-  visits = np.array([c['visits'] for c in candidates])
+    return -1
+  winrates = np.array(player['winrate'])[candidates]
+  visits = np.array(player['visits'])[candidates]
   weights = winrates / (1 + visits); weights = weights / weights.sum()
   decision = np.random.choice(candidates, 1, p = weights)[0]
   return decision
 
-def play():
-  start = np.random.choice(data, 1)[0]['last']
+### initialize a player
+player = {'winrate': [0.5] * N, 'visits': [0] * N}
+
+### play
+def self_play(player):
+  start = np.random.choice(lasts, 1)[0]
   sequence = []
   p = start
   while True:
-    idiom = choose(p, sequence)
-    if idiom == 0:
+    i = choose(p, player, sequence)
+    if i == -1:
       break
-    sequence.append(idiom)
-    print(idiom['word'])
-    p = idiom['last']
+    sequence.append(i)
+    print(words[i])
+    p = lasts[i]
   return sequence
-  
+
+def learn(player, sequence):
+  evens, odds = sequence[::2], sequence[1::2]
+  if len(sequence)%2 == 0:
+    wins, loses = odds, evens
+  else:
+    wins, loses = evens, odds
+  return(wins)
+
+
+
+
+
