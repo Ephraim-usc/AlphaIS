cdde25d8849eeb3d96ce02c4d303dfa233d30652
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-01-27 15:49:21 -0800

    Update ais.py

--- ais.py
+++ ais.py
@@ -8,8 +8,9 @@ words = [d['word'] for d in data]
 pinyins = [d['pinyin'] for d in data]
 firsts = [p.split()[0] for p in pinyins]
 lasts = [p.split()[-1] for p in pinyins]
-
 pinyins = set(firsts + lasts)
+M = len(pinyins)
+
 index = {}
 for pinyin in pinyins:
   index[pinyin] = []
@@ -17,19 +18,28 @@ for pinyin in pinyins:
     if firsts[i] == pinyin:
       index[pinyin].append(i)
 
-def choose(key, player, excludes):
+back_index = {}
+for pinyin in pinyins:
+  back_index[pinyin] = []
+  for i in range(N):
+    if lasts[i] == pinyin:
+      back_index[pinyin].append(i)
+
+def choose(key, player, excludes = []):
   candidates = index[key]
   canditates = set(candidates) - set(excludes)
   if candidates == []:
     return -1
-  winrates = np.array(player['winrate'])[candidates]
-  visits = np.array(player['visits'])[candidates]
+  winrates = np.array([player['winrate'][lasts[i]] for i in canditates])
+  visits = np.array([player['visits'][lasts[i]] for i in canditates])
   weights = winrates / (1 + visits); weights = weights / weights.sum()
   decision = np.random.choice(candidates, 1, p = weights)[0]
   return decision
 
 ### initialize a player
-player = {'winrate': [0.5] * N, 'visits': [0] * N}
+def new_player():
+  buffer = {'winrate': {p:0.5 for p in pinyins}, 'visits': {p:2 for p in pinyins}}
+  return buffer
 
 ### play
 def self_play(player):
@@ -37,11 +47,11 @@ def self_play(player):
   sequence = []
   p = start
   while True:
-    i = choose(p, player, sequence)
+    i = choose(p, player)
     if i == -1:
       break
     sequence.append(i)
-    print(words[i])
+    #print(words[i])
     p = lasts[i]
   return sequence
 
@@ -51,9 +61,43 @@ def learn(player, sequence):
     wins, loses = odds, evens
   else:
     wins, loses = evens, odds
-  return(wins)
+  
+  for i in wins:
+    p = lasts[i]
+    player['winrate'][p] = (player['winrate'][p] * player['visits'][p] + 1) / (player['visits'][p] + 1)
+    player['visits'][p] += 1
+  
+  for i in loses:
+    p = lasts[i]
+    player['winrate'][p] = (player['winrate'][p] * player['visits'][p]) / (player['visits'][p] + 1)
+    player['visits'][p] += 1
 
+def train(player, n):
+  for i in range(n):
+    s = self_play(player)
+    learn(player, s)
+    print(i)
 
-
-
+### play
+def play(player1, player2):
+  start = np.random.choice(lasts, 1)[0]
+  sequence = []
+  p = start
+  print("start pinyin: " + str(p))
+  while True:
+    i = choose(p, player1)
+    if i == -1:
+      break
+    sequence.append(i)
+    p = lasts[i]
+    print("player 1 gives: " + str(words[i]))
+    print("player 1 current win rate: " + str(player1['winrate'][p]))
+    i = choose(p, player2)
+    if i == -1:
+      break
+    sequence.append(i)
+    p = lasts[i]
+    print("player 2 gives: " + str(words[i]))
+    print("player 2 current win rate: " + str(player2['winrate'][p]))
+  return sequence
 
