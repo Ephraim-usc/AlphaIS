7b6dd31dd3a4bd30916bf8d0e781dea61cf258a6
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-01-27 13:23:15 -0800

    Update ais.py

--- ais.py
+++ ais.py
@@ -1,3 +1,57 @@
 import json
+import pickle
+import numpy
 
 data = json.load(open("idiom.json", "r"))
+idioms = {}
+for d in data:
+  d['first'] = d['pinyin'].split()[0]
+  d['last'] = d['pinyin'].split()[-1]
+  idioms[d['word']] = d
+
+pinyins = []
+for i in idioms.values():
+  pinyins.append(i['first'])
+  pinyins.append(i['last'])
+
+pinyins = set(pinyins)
+
+index = {}
+for pinyin in pinyins:
+  index[pinyin] = []
+  for i in idioms.values():
+    if i['first'] == pinyin:
+      index[pinyin].append(i)
+
+def find(idex, key):
+  results = index[key]
+  for r in results:
+    print(r['word'])
+
+### initialize
+for d in data:
+  d['winrate'] = 0.5
+  d['visits'] = 0
+
+def choose(key):
+  candidates = index[key]
+  if candidates == []:
+    return 0
+  winrates = np.array([c['winrate'] for c in candidates])
+  visits = np.array([c['visits'] for c in candidates])
+  weights = winrates / (1 + visits); weights = weights / weights.sum()
+  decision = np.random.choice(candidates, 1, p = weights)[0]
+  return decision
+
+def play():
+  start = np.random.choice(data, 1)[0]['last']
+  sequence = []
+  p = start
+  while True:
+    idiom = choose(p)
+    if idiom == 0:
+      break
+    sequence.append(idiom)
+    print(idiom['word'])
+  return sequence
+  
